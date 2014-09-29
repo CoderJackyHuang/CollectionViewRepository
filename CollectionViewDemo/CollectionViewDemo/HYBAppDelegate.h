d4fa63e4ba0ff1cbc3b4a3d032e14e6a1d5b30dd
ljy-335 <ljy-335@biaoge.local>
2014-09-29 16:36:23 +0800

    Create a project

---
+++ CollectionViewDemo/CollectionViewDemo/HYBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HYBAppDelegate.h
+//  CollectionViewDemo
+//
+//  Created by ljy-335 on 14-9-29.
+//  Copyright (c) 2014年 uni2uni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HYBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewDemo/CollectionViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionViewDemo
+//
+//  Created by ljy-335 on 14-9-29.
+//  Copyright (c) 2014年 uni2uni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HYBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HYBAppDelegate class]));
+    }
+}
